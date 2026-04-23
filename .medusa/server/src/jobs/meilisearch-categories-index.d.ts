f0f0c43e7faafbeeb76394132273506c8ed71806
Maksym <102756985+KohutiakMaksym@users.noreply.github.com>
2026-04-23 16:23:01 +0100

    removed indexing on startup

--- .medusa/server/src/jobs/meilisearch-categories-index.d.ts
+++
@@ -1,5 +0,0 @@
-import { MedusaContainer } from '@medusajs/framework';
-import { CronJobConfig } from '../models/CronJobConfig';
-export default function meilisearchCategoriesIndexJob(container: MedusaContainer): Promise<void>;
-export declare const config: CronJobConfig;
-//# sourceMappingURL=meilisearch-categories-index.d.ts.map
